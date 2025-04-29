7d01200a32ee163355cc151d0883a09c702c7098
Matias <lit.io30303@gmail.com>
2025-04-29 14:37:33 -0400

    enrutamiento  #3

--- Public/index.php
+++ Public/index.php
@@ -1,7 +1,16 @@
 <?php
-    //require_once __DIR__ . '/../App/autoload.php';
-    set_include_path(__DIR__ . '/../App/');
-    require_once 'autoload.php';
+    //require_once __DIR__ . '/../app/autoload.php';
+    //set_include_path(__DIR__ . '/../app/');
+    //require_once 'autoload.php';
+    // Construir una ruta absoluta hacia autoload.php
+    $autoloadPath = realpath(__DIR__ . '/../app/autoload.php');
+
+    // Verificar que el archivo realmente existe
+    if (!file_exists($autoloadPath)) {
+        throw new Exception("El archivo autoload.php no se encuentra en: $autoloadPath");
+    }
+
+    require_once $autoloadPath;
 
     
     $router = new Router();
