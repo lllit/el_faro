cf10389e8e668e02b5c9c727c5229b102dc1f08f
Matias <lit.io30303@gmail.com>
2025-04-28 21:16:45 -0400

    change favicon psition

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -7,7 +7,7 @@
   <title>El Faro</title>
   <link rel="stylesheet" href="/style.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/all.min.js"></script>
-  <link rel="icon" href="/imagenes/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="favicon.ico" type="image/x-icon">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
 </head>
 
