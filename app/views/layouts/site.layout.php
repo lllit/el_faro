d1a34f495d5b0b9372ea2e1707f7914e5852ff07
Matias <lit.io30303@gmail.com>
2025-04-29 16:50:54 -0400

    eliminando segundo slash head nav

--- app/views/layouts/site.layout.php
+++ app/views/layouts/site.layout.php
@@ -6,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>El Faro</title>
     
-    <link rel="stylesheet" href="<?= URL_PATH ?>/Assets/css/style.css">
-    <script src="<?= URL_PATH ?>/Assets/js/main.js"></script>
-    <link rel="icon" href="<?= URL_PATH ?>/Assets/favicon/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="<?= URL_PATH ?>Assets/css/style.css">
+    <script src="<?= URL_PATH ?>Assets/js/main.js"></script>
+    <link rel="icon" href="<?= URL_PATH ?>Assets/favicon/favicon.ico" type="image/x-icon">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/all.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
 </head>
