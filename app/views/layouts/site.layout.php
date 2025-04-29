9f95b6b0c32d7ca926a3fbba0ac82bef0244935a
Matias <lit.io30303@gmail.com>
2025-04-29 16:37:52 -0400

    update

--- app/views/layouts/site.layout.php
+++ app/views/layouts/site.layout.php
@@ -7,8 +7,8 @@
     <title>El Faro</title>
     
     <link rel="stylesheet" href="<?= URL_PATH ?>public/Assets/css/style.css">
-    <script src="<?= URL_PATH ?>Assets/js/main.js"></script>
-    <link rel="icon" href="<?= URL_PATH ?>/Assets/favicon/favicon.ico" type="image/x-icon">
+    <script src="<?= URL_PATH ?>public/Assets/js/main.js"></script>
+    <link rel="icon" href="<?= URL_PATH ?>public//Assets/favicon/favicon.ico" type="image/x-icon">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/all.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
 </head>
