9e6fb8d2771106dba037ac4d3e888af2925f3b40
Matias <lit.io30303@gmail.com>
2025-04-29 14:15:08 -0400

    - change index - add vercelignore

--- Public/index.php
+++ Public/index.php
@@ -1,5 +1,8 @@
 <?php
-    require_once __DIR__ . '/../App/autoload.php';
+    //require_once __DIR__ . '/../App/autoload.php';
+    set_include_path(__DIR__ . '/../App/');
+    require_once 'autoload.php';
+
     
     $router = new Router();
     $router->run();
